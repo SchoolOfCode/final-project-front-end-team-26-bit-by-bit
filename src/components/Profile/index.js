cd4e333e61a35a2e7940a8c8cd8ae7c2b9638cb6
Quinn.C <joanchenuk@gmail.com>
2022-02-22 12:20:03 +0000

    menu icon added in profile page

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
+
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
@@ -9,6 +10,7 @@ const Profile = () => {
   }
 
   return (
+    
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
