aeca1115c4de8a18e56c4ee35c5aa1a1ad12f629
Quinn.C <joanchenuk@gmail.com>
2022-02-22 10:23:11 +0000

    fetch get request added in reminder

--- src/components/Reminders/index.js
+++ src/components/Reminders/index.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect,useState } from "react";
 import ReminderItem from "../ReminderItem";
 import "./Reminders.css"
 import { FaPlus } from "react-icons/fa";
 const Reminders = () =>{
+    const[reminderData, setReminderData]=useState('');
+
+    async function fetchReminders(){
+        const data = await fetch(`https://simple-room26.herokuapp.com/users/1/reminders`);
+        const response = await data.json();
+        console.log(response);
+
+
+    }
+    useEffect(()=>{
+        
+        setReminderData(fetchReminders());
+    }, []);
+    console.log(reminderData);
     let items=["a","b","c","d"]
     return(
         <div className="Blue">
