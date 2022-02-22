1b91588b2a8d61747a73cd0c4a139ab212c5d313
NicholasGomis <nicholasgomis@icloud.com>
2022-02-22 16:05:10 +0000

    Fixed SignInButton removed extra sign and logout buttons

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Dashboard from "../Dashboard";
 import ProfilePage from "../ProfilePage";
-
+import { SignInPage}  from "../SignInPage";
 import SignInButton from "../SignInButton";
 import SignOutButton from "../SignOutButton";
 import { Routes,Route } from "react-router-dom";
@@ -10,14 +10,15 @@ import { FaSkiingNordic } from "react-icons/fa";
 function App() {
   return (
     <div className="App">
-       
-    <SignOutButton/>
-    <SignInButton/>
+{/*        
+    <SignOutButton/> */}
+  
      
         
       <Routes>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/profile" element={<ProfilePage/>}/>
+          <Route path="/" element={<SignInPage/>}/>
       </Routes>
      
     </div>
---
+++ src/components/SignInPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignInButton from '../SignInButton'
+
+export function SignInPage(props) {
+    
+
+    return (
+    <div className="SignInPage">
+      <h1> This is our sign in page</h1>
+        <SignInButton/>
+     
+    </div>
+    )
+}
