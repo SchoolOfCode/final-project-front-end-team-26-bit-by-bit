206ef16e56278ab13636ded4feaa5cca53077077
Quinn.C <joanchenuk@gmail.com>
2022-02-22 10:55:16 +0000

    sign in page route done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Dashboard from "../Dashboard";
+import SignIn from "../SignIn";
 import { Routes,Route } from "react-router-dom";
+import { FaSkiingNordic } from "react-icons/fa";
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
 
       <Routes>
         <Route path="/dashboard" element={<Dashboard/>}/>
+        
+      </Routes>
+      <Routes>
+        <Route path="/" element={<SignIn/>}/>
         
       </Routes>
     </div>
---
+++ src/components/SignIn/index.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+
+function SignIn() {
+  return (
+    <div className="SignIn">
+      <h1>Sign in</h1>
+    </div>
+  );
+}
+
+export default SignIn;
