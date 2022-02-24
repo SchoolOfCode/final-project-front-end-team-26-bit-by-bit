2b191ee416b36348d36676f3231a5d031b21afcf
NicholasGomis <nicholasgomis@icloud.com>
2022-02-24 10:03:09 +0000

    Styled the sign in btn and met MVP for the sign in page

--- src/components/SignInButton/index.js
+++ src/components/SignInButton/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import './SignInButton.css' 
 
 const SignInButton = () => {
   const { user, loginWithRedirect } = useAuth0();
 console.log(user);
-  return <button onClick={() => loginWithRedirect()}>Sign in</button>;
+  return <button className="sign-btn" onClick={() => loginWithRedirect()}>Sign in</button>;
 };
 
 export default SignInButton;
