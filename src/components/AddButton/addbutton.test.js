c8c4342586aef5faabad1f54b1449105505d223b
Quinn.C <joanchenuk@gmail.com>
2022-03-02 16:22:04 +0000

    add button, dashboard test done

---
+++ src/components/AddButton/addbutton.test.js
@@ -0,0 +1,12 @@
+import {render, screen} from '@testing-library/react';
+import AddTodoListButton from './index.js';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+
+
+describe('<AddToDoListButton /> rendering', () => {
+    it('render AddToDoListButton Component without crashing', () => {
+      //testing a single component without child components
+        shallow(<AddTodoListButton/>);
+    });})
