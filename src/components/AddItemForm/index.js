5e4a3d3611ce1c752a1100ade831bb7fdd505d1c
Katherine Tweeddale <Katweeddale@gmail.com>
2022-03-03 10:25:23 +0000

    buttons

--- src/components/AddItemForm/index.js
+++ src/components/AddItemForm/index.js
@@ -7,140 +7,136 @@ import { useAuth0 } from "@auth0/auth0-react";
 function AddItemForm() {
   const location = useLocation();
   let page = location.state;
-  
+
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   const [user_id, setUser_id] = useState(Number(user.sub.substring(14, 18)));
-  const [isActive, setIsActive]  = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
+  const [text, setText] = useState("");
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
+  const [priority, setPriority] = useState("");
+  const [isCompleted, setIsCompleted] = useState(false);
+  const [isMonday, setIsMonday] = useState(false);
+  const [isTuesday, setIsTuesday] = useState(false);
+  const [isWednesday, setIsWednesday] = useState(false);
+  const [isThursday, setIsThursday] = useState(false);
+  const [isFriday, setIsFriday] = useState(false);
+  const [isSaturday, setIsSaturday] = useState(false);
+  const [isSunday, setIsSunday] = useState(false);
 
-  const [text, setText] = useState('');
-  const [time, setTime] = useState('');
-  const [date, setDate] = useState('');
-  const [priority, setPriority] = useState('');
-
- const [isMonday, setIsMonday] = useState(false)
- const [isTuesday, setIsTuesday] = useState(false)
- const [isWednesday, setIsWednesday] = useState(false)
- const [isThursday, setIsThursday] = useState(false)
- const [isFriday, setIsFriday] = useState(false)
- const [isSaturday, setIsSaturday] = useState(false)
- const [isSunday, setIsSunday] = useState(false)
-
-
- function handleMonday() {
-  setIsMonday(!isMonday)
-}
-
-function handleTuesday() {
-  setIsTuesday(!isTuesday)
-}
-
-  
-  function handleWednesday() {
-    setIsWednesday(!isWednesday)
+  function handleActive() {
+    setIsActive(!isActive);
   }
-    //   
-    function handleThursday() {
-      setIsThursday(!isThursday)
-    }
-      //   
-      function handleFriday() {
-        setIsFriday(!isFriday)
-      }
-        //   
-        function handleSaturday() {
-          setIsSaturday(!isSaturday)
-        }
-          //   
-function handleSunday() {
-            setIsSunday(!isSunday)
-          }
-            //   
-  //   function recurringCheck() {
-  //     console.log("clicked");
-  //     const ratio = document.getElementsByClassName("recurringCheck")[0];
-  //     if (ratio.style.backgroundColor === "red") {
-  //       ratio.style.backgroundColor = "#A3F596";
-  //     } else {
-  //       ratio.style.backgroundColor = "red";
-  //     }
-  //   }
+  function handleMonday() {
+    setIsMonday(!isMonday);
+  }
+
+  function handleTuesday() {
+    setIsTuesday(!isTuesday);
+  }
+
+  function handleWednesday() {
+    setIsWednesday(!isWednesday);
+  }
+  //
+  function handleThursday() {
+    setIsThursday(!isThursday);
+  }
+  //
+  function handleFriday() {
+    setIsFriday(!isFriday);
+  }
+  //
+  function handleSaturday() {
+    setIsSaturday(!isSaturday);
+  }
+  //
+  function handleSunday() {
+    setIsSunday(!isSunday);
+  }
+  //
 
   function reminderClick() {
     fetchPostRem();
-    setDate('');
-    setText('');
-    setTime('');
+    setDate("");
+    setText("");
+    setTime("");
   }
-    
+
+  async function fetchPostRem() {
     let reminder_id = Math.floor(1000 + Math.random() * 9000);
-    async function fetchPostRem() {
-      let response = await fetch(
-        `https://simple-room27.herokuapp.com/users/${user_id}/reminders`,
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            user_id: user_id,
-            reminder_id: reminder_id,
-            text: text,
-            due_date: date,
-            time: time,
-            iscompleted: false
-          })
-        }
-      );
-      let data = await response.json();
-      console.log("post dp", data);
-    }
-    
+    let response = await fetch(
+      `https://simple-room27.herokuapp.com/users/${user_id}/reminders`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: user_id,
+          reminder_id: reminder_id,
+          text: text,
+          due_date: date,
+          time: time,
+          iscompleted: false,
+        }),
+      }
+    );
+    let data = await response.json();
+    console.log("post dp", data);
+  }
 
   // user_id int,
 
-
   function todoClick() {
-    fetchPostTodos()
-      setText('');
-      setTime('');
+    fetchPostTodos();
+    setText("");
+    setTime("");
+    setPriority("low");
+    setIsCompleted(false);
+    setIsMonday(false);
+    setIsTuesday(false);
+    setIsWednesday(false);
+    setIsThursday(false);
+    setIsFriday(false);
+    setIsSaturday(false);
+    setIsSunday(false);
   }
 
+  async function fetchPostTodos() {
     let todo_id = Math.floor(1000 + Math.random() * 9000);
-
-    console.log(text, todo_id, priority, time, user_id)
-    async function fetchPostTodos() {
-      let response = await fetch(
-        `https://simple-room27.herokuapp.com/users/${user_id}/todo`,
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            user_id: user_id,
-            todo_id: todo_id,
-            text: text,
-            priority: priority,
-            time: time,
-            isMonday: isMonday,
-            isTuesday: isTuesday,
-            isWednesday: isWednesday,
-            isThursday: isThursday,
-            isFriday: isFriday,
-            isSaturday: isSaturday,
-            isSunday: isSunday,
-            iscompleted: false
-          })
-        })
-        let data = await response.json();
-        console.log("post dp", data);
+    let response = await fetch(
+      `https://simple-room27.herokuapp.com/users/${user_id}/todo`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user_id: user_id,
+          todo_id: todo_id,
+          text: text,
+          priority: priority,
+          time: time,
+          isMonday: isMonday,
+          isTuesday: isTuesday,
+          isWednesday: isWednesday,
+          isThursday: isThursday,
+          isFriday: isFriday,
+          isSaturday: isSaturday,
+          isSunday: isSunday,
+          iscompleted: isCompleted,
+        }),
       }
-      
-      
+    );
+    let data = await response.json();
+    console.log("post dp", data);
+  }
+
   if (page === "Reminders") {
     return (
       <div>
@@ -153,9 +149,10 @@ function handleSunday() {
               required
               placeholder="Task name"
               onChange={(event) => {
-                setText(event.target.value)
+                setText(event.target.value);
               }}
-             value={text}></input>
+              value={text}
+            ></input>
           </div>
           <div className="InpToDo">
             <h3>Date</h3>
@@ -164,9 +161,10 @@ function handleSunday() {
               placeholder="YYYY/MM/DD"
               type="date"
               onChange={(event) => {
-               setDate(event.target.value)
+                setDate(event.target.value);
               }}
-            value={date}></input>
+              value={date}
+            ></input>
           </div>
           <div className="InpToDo">
             <h3>Time</h3>
@@ -179,9 +177,10 @@ function handleSunday() {
                 // type="time"
                 placeholder="Time"
                 onChange={(event) => {
-                 setTime(event.target.value)
+                  setTime(event.target.value);
                 }}
-              value={time}></input>
+                value={time}
+              ></input>
             </div>
           </div>
         </form>
@@ -198,50 +197,82 @@ function handleSunday() {
         <Header bool={"form"} />
         <form className="BlueForm">
           <h2 className="TitleForm">Add To Do</h2>
+
           <div className="InpToDo">
             <h3>Task Name</h3>
             <input
               required
               placeholder="Task name"
               onChange={(event) => {
-                setText(event.target.value)
+                setText(event.target.value);
               }}
-            value={text}></input>
+              value={text}
+            ></input>
           </div>
-          <div className="InpToDo">
-          <h3>Time</h3>
-              <input
-                required
-                type="number"
-                min="0"
-                max="24"
-                // type="time"
-                placeholder="Time"
-                onChange={(event) => {
-                  //   let intTime = Number(
-                  //     String(event.target.value).substring(0, 2)
-                  //   );
-                  //   setTime(intTime);
-                  setTime(event.target.value)
-                }}
-              value={time}></input>
-            </div>
 
-            <div className="InpToDo">
+          <div className="InpToDo">
+            <h3>Time</h3>
+            <input
+              required
+              type="number"
+              min="0"
+              max="24"
+              // type="time"
+              placeholder="Time"
+              onChange={(event) => {
+                //   let intTime = Number(
+                //     String(event.target.value).substring(0, 2)
+                //   );
+                //   setTime(intTime);
+                setTime(event.target.value);
+              }}
+              value={time}
+            ></input>
+          </div>
+
+          <div className="InpToDo">
             <h3>Reoccuring</h3>
-            
-            <div onClick={()=>{setIsActive(!isActive)}}>Choose day</div>
+
+            <button onClick={handleActive}>Choose day</button>
+
             {isActive ? (
-                <div>
-            <button className={String(isMonday)} onClick={handleMonday}> Monday </button>
-            <button className={String(isTuesday)} onClick={handleTuesday}> Tuesday </button>
-            <button className={String(isWednesday)} onClick={handleWednesday}> Wednesday </button>
-            <button className={String(isThursday)} onClick={handleThursday}> Thursday </button>
-            <button className={String(isFriday)} onClick={handleFriday}> Friday </button>
-            <button className={String(isSaturday)} onClick={handleSaturday}> Saturday </button>
-            <button className={String(isSunday)} onClick={handleSunday}> Sunday </button>
-</div>) : ( null ) }
-</div>
+              <div>
+                <button className={String(isMonday)} onClick={handleMonday}>
+                  {" "}
+                  Monday{" "}
+                </button>
+                <button className={String(isTuesday)} onClick={handleTuesday}>
+                  {" "}
+                  Tuesday{" "}
+                </button>
+                <button
+                  className={String(isWednesday)}
+                  onClick={handleWednesday}
+                >
+                  {" "}
+                  Wednesday{" "}
+                </button>
+                <button className={String(isThursday)} onClick={handleThursday}>
+                  {" "}
+                  Thursday{" "}
+                </button>
+                <button className={String(isFriday)} onClick={handleFriday}>
+                  {" "}
+                  Friday{" "}
+                </button>
+                <button className={String(isSaturday)} onClick={handleSaturday}>
+                  {" "}
+                  Saturday{" "}
+                </button>
+                <button className={String(isSunday)} onClick={handleSunday}>
+                  {" "}
+                  Sunday{" "}
+                </button>
+              </div>
+            ) : (
+              <div></div>
+            )}
+          </div>
 
           <div className="InpToDo">
             <h3>Urgency</h3>
@@ -251,8 +282,7 @@ function handleSunday() {
                 type="button"
                 id="red"
                 onClick={(e) => {
-                  setPriority(e.target.id)
-
+                  setPriority(e.target.id);
                 }}
               ></button>
               <button
@@ -260,7 +290,7 @@ function handleSunday() {
                 type="button"
                 id="yellow"
                 onClick={(e) => {
-                  setPriority(e.target.id)
+                  setPriority(e.target.id);
                 }}
               ></button>
               <button
@@ -268,24 +298,17 @@ function handleSunday() {
                 type="button"
                 id="green"
                 onClick={(e) => {
-                  setPriority(e.target.id)
+                  setPriority(e.target.id);
                 }}
               ></button>
             </div>
           </div>
-
         </form>
         <Link to="/dashboard">
-          <button
-            type="submit"
-            className="submitForm"
-            onClick={todoClick}
-          >
+          <button type="submit" className="submitForm" onClick={todoClick}>
             Submit
           </button>
-
         </Link>
-
       </div>
     );
   }
